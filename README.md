0619b580bbb6d9dbf1a087508065d10f0061bfe2
Erik Hulmak <erik@hulmak.cz>
2023-02-12 11:01:11 +0100

    recording

--- README.md
+++ README.md
@@ -1,16 +1,10 @@
 # 0001-game-of-life
 In this project, we aim to turn John Conway's game of life into a music instrument. 
 
-TODO: add link to the video
+Thank you for attention :)
 
-![](preview.gif)
+Polished version of this code will be available on my GitHub
 
+please check the description, there will be links and everything.
 
-# To replicate
-
-Download the OB-Xd synth - https://www.discodsp.com/obxd/
-- to use the synth on mac, you have to first 
-- Run the synth. On mac you have to first add port in the MIDI Studio: https://support.apple.com/guide/audio-midi-setup/transfer-midi-information-between-apps-ams1013/mac
-- in the OB-Xd you can change presets in the menu. Select "Bank" (set of programs) and "program" which is a preset. You can also try to change the sounds with turning the knobs.
-
-Enjoy :]
+Bye! :]
--- src/controller_midi.py
+++ src/controller_midi.py
@@ -1,132 +1,122 @@
-from cellular_automata import GameOfLife
 import pygame
 import pygame.midi
 import numpy as np
+import yaml
+from cellular_automata import GameOfLife
+from time import sleep
 import sys
 import datetime
 
 
-GRID_COLOR = (30, 30, 30)
-COLORS = [(0, 0, 0), (255, 255, 255)]
-
-
 """
 Controls:
-    SPACE: start/stop
-    s: save board
-    r: reset - load saved board
-    CTRL+s: save board as file (filename: game_of_life_<datetime>.npy)
-    RIGHT ARROW: step
-    c: clear board
-    q: quit
-    ESC: quit
-    mouse: draw (toggle, drag)
-
-
-if you want to load the game from the file, instead of "run_game_of_life()" in the __main__ section,
-use: load_game_of_life("filepath"), where filepath is the path to the file with the saved board.
+    SPACE - pause/resume
+    RIGHT - step
+    c - clear
+    r - load saved state (before SPACE)
+    q - quit
+    ESC - quit
+    mouse - toggle cell, draw
 """
 
-class ControllerMidi:
-    def __init__(self, game: GameOfLife, cellsize=10):
-        self.game = game
-        self.running = False
-        self.drawing = False
-        self.mouse_cell = (0, 0)
-        self.saved_board = self.game.board.copy()
-        self.H, self.W = game.board.shape
-        self.S = cellsize
-        self.current_state = np.ones_like(game.board, dtype=bool)
+class AttrDict(dict):
+    def __init__(self, *args, **kwargs):
+        super(AttrDict, self).__init__(*args, **kwargs)
+        self.__dict__ = self
 
+
+class Controller:
+    def __init__(self, game: GameOfLife, conf: AttrDict):
+        self.conf = conf
+        self.game = game
         pygame.init()
-        pygame.midi.init()
+        self.canvas = pygame.display.set_mode((conf.W * conf.CELL_SIZE, conf.H * conf.CELL_SIZE))
         pygame.display.set_caption("John Conway's Game of Life")
+        self.clock = pygame.time.Clock()
+
+        pygame.midi.init()
         port = pygame.midi.get_default_output_id()
         self.midi_out = pygame.midi.Output(port, 0)
-        self.canvas = pygame.display.set_mode((self.W * self.S, self.H * self.S))
-        self.clock = pygame.time.Clock()
-        # -----
-        self.canvas.fill(GRID_COLOR)
-        self.draw_play(np.logical_not(self.current_state))   # empty board, all notes off
-        self.draw_play()
 
-
+        self.saved_state = game.board.copy()
+        self.current_state = np.ones_like(game.board, dtype=bool)
+        self.canvas.fill(self.conf.GRID_COLOR)
+        self.draw(np.logical_not(self.current_state))  # empty board
+        self.draw()  # draw the initial state
+        self.running = False
+        self.drawing = False
+        self.mouse_cell = (-1, -1)
+    
     def quit(self):
-        for n in range(127):
-            self.midi_out.note_off(note=n, velocity=127, channel=0)
-        self.midi_out.abort()
-        self.midi_out.close()
-        pygame.midi.quit()
         pygame.quit()
         sys.exit(0)
     
+    def draw(self, board=None):
+        board = board if board is not None else self.game.board
+        S, G = self.conf.CELL_SIZE, self.conf.GAP
+        msg = [self.midi_out.note_off, self.midi_out.note_on]
+
+        # draw only the cells that changed
+        for y, x in zip(*np.where(board != self.current_state)):
+            msg[board[y, x]](note=x%128, velocity=(127-y)%128, channel=0)
+            pygame.draw.rect(
+                self.canvas, 
+                self.conf.DEAD_LIVE_COLOR[board[y, x]],
+                (x*S, y*S, S-G, S-G),
+                )
+        self.current_state = board.copy()
+        pygame.display.update()
+
     def get_cell(self):
         x, y = pygame.mouse.get_pos()
-        return x // self.S, y // self.S
-
-    def control(self, event: pygame.event):
+        return x // self.conf.CELL_SIZE, y // self.conf.CELL_SIZE
+    
+    def control(self, event):
         if event.type == pygame.QUIT:
             self.quit()
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
+            if event.key == pygame.K_ESCAPE or event.key == pygame.K_q:
+                self.quit()
+            elif event.key == pygame.K_SPACE:
                 if not self.running:
-                    self.board_before_run = self.game.board.copy()
+                    self.saved_state = self.game.board.copy()
                 self.running = not self.running
             elif event.key == pygame.K_RIGHT:
                 self.step()
-            elif event.key == pygame.K_ESCAPE or event.key == pygame.K_q:
-                self.quit()
-            elif event.key == pygame.K_r:
-                if self.running:
-                    self.running = False
-                self.game.board = self.saved_board.copy()
-                self.draw_play()
             elif event.key == pygame.K_c:
+                for i in range(128):
+                    self.midi_out.note_off(note=i, velocity=127, channel=0)
                 self.game.clear()
-                self.draw_play()
-            elif event.key == pygame.K_s:
-                if pygame.key.get_mods() & pygame.KMOD_CTRL:
-                    filename = f"game_of_life_{datetime.datetime.now().isoformat()}.npy"
-                    self.game.save(filename)
-                    print(f'saved as: "{filename}"')
-                else:
-                    self.saved_board = self.game.board.copy()
-                    print("board saved")
+                self.draw()
+            elif event.key == pygame.K_r:
+                self.game.board = self.saved_state.copy()
+                self.draw()
+        elif event.type == pygame.K_s:
+            if pygame.key.get_mods() & pygame.KMOD_CTRL:
+                filename = f"game_of_life_{datetime.datetime.now().isoformat()}.npy"
+                self.game.save(filename)
+                print(f'saved as: "{filename}"')
         elif event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
                 self.drawing = True
-                self.mouse_cell = self.get_cell()
-                self.game.toggle_cell(*self.mouse_cell)
-                self.draw_play() 
+                x, y = self.get_cell()
+                self.game.toggle_cell(x, y)
+                self.draw()
         elif event.type == pygame.MOUSEBUTTONUP:
             if event.button == 1:
                 self.drawing = False
-                self.draw_play() 
+                self.draw()
         elif event.type == pygame.MOUSEMOTION:
             if self.drawing:
                 cell = self.get_cell()
                 if cell != self.mouse_cell:
                     self.mouse_cell = cell
                     self.game.toggle_cell(*cell)
-                    self.draw_play()
+                    self.draw()
     
-    def draw_play(self, board = None):
-        board = board if board is not None else self.game.board
-        f = [self.midi_out.note_off, self.midi_out.note_on]
-
-        for y, x in zip(*np.where(board != self.current_state)):
-            f[board[y, x]](note=x%128, velocity=(127-y)%128, channel=0)
-            pygame.draw.rect(
-                self.canvas, 
-                COLORS[board[y, x]], 
-                (x*self.S, y*self.S, self.S-1, self.S-1)
-                )
-        self.current_state = board.copy()
-        pygame.display.update()
-
     def step(self):
         self.game.step()
-        self.draw_play()
+        self.draw()
     
     def run(self):
         while True:
@@ -134,20 +124,17 @@ class ControllerMidi:
                 self.control(event)
             if self.running:
                 self.step()
-            self.clock.tick(30)
+            self.clock.tick(self.conf.FPS)
 
-
-def run_game_of_life(W=100, H=50):
-    game = GameOfLife(np.zeros((H, W), dtype=bool))
-    controler = ControllerMidi(game, cellsize=20)
-    controler.run()
-
-
-def load_game_of_life(filepath: str):
-    game = GameOfLife(filepath)
-    controler = ControllerMidi(game, cellsize=20)
-    controler.run()
+def main():
+    with open("src/config.yaml", "r") as f:
+        config = AttrDict(yaml.safe_load(f))
+    # board = np.random.randint(0, 2, size=(config.H, config.W))
+    board = np.zeros((config.H, config.W), dtype=bool)
+    game = GameOfLife(board)
+    controller = Controller(game, config)
+    controller.run()
 
 
 if __name__ == "__main__":
-    run_game_of_life()
+    main()
